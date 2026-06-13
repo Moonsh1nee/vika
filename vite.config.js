9310f2818a154b019ad291dbb541fa4ff92600d5
Moonsh1ne <moonsh1ne.vlad@yandex.ru>
2026-06-13 18:45:57 +0300

    Fix2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/vika/',
   server: {
     host: '127.0.0.1',
     port: 5173,
